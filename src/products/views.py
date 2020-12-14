c88964e157fe1054f41a4feb1263d0edab409b5c
Dhirendra Kashiwale <kashiwale@yahoo.com>
2020-12-14 10:58:44 -0500

    Sending Data to view

--- src/products/views.py
+++ src/products/views.py
@@ -8,6 +8,16 @@ def chart_select_view(request):
     purchase_df = pd.DataFrame(Purchase.objects.all().values())
     product_df['product_id']=product_df['id']
     df = pd.merge(purchase_df, product_df, on='product_id').drop(['id_y','date_y'], axis=1).rename({'id_x': 'id', 'date_x': 'date'},axis = 1)
+    if request.method == 'POST':
+        #chart_type = request.POST.get('sales')
+        print(request.POST)
+        chart_type = request.POST['sales']
+        date_from = request.POST['date_from']
+        date_to = request.POST['date_to']
+
+        print(chart_type)
+        print(date_from ,'-----', date_to )
+
     context = {
         'products' : product_df.to_html(),
         'purchase' : purchase_df.to_html(),
