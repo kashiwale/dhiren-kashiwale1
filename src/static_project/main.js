a1e10048ccbf3a4414ff6033d5c6b2e5e3e2d6f2
Dhirendra Kashiwale <kashiwale@yahoo.com>
2020-12-14 19:28:15 -0500

    Closing the error Message

--- src/static_project/main.js
+++ src/static_project/main.js
@@ -3,3 +3,12 @@ $(document).ready(function(){
         .dropdown()
 ;
 })
+
+$('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  })
+;
