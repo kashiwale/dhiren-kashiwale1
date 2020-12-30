db1c4a30160ddbfa2a53a0223d20400a9b72e476
Dhirendra Kashiwale <kashiwale@yahoo.com>
2020-12-30 18:07:05 -0500

    add postgres database

--- src/dj_pd/settings.py
+++ src/dj_pd/settings.py
@@ -80,8 +80,14 @@ WSGI_APPLICATION = 'dj_pd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dj_pd',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
