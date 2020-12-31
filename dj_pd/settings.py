67482a1d875b309e865f840011edc01cfdbe1b8d
Dhirendra Kashiwale <kashiwale@yahoo.com>
2020-12-30 22:28:08 -0500

    added dir in template of settings file

--- dj_pd/settings.py
+++ dj_pd/settings.py
@@ -58,7 +58,10 @@ ROOT_URLCONF = 'dj_pd.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR / 'templates')],
+        'DIRS': [os.path.join(BASE_DIR / 'templates'),
+                os.path.join(BASE_DIR / 'csvs/templates'),
+        
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
