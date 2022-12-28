6679bb754961923807b6b290aa08f5ae7a24872b
kashiwale <kashiwale@yahoo.com>
2022-12-28 16:21:45 -0500

    update settings with creds

--- dj_pd/settings/production.py
+++ dj_pd/settings/production.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','dhiren-kashiwale.herokuapp.com']
 
